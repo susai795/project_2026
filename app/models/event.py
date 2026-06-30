672dee57fe280490ff845431909ecb5bc51a6c59
susai795 <s.usai151@studenti.unica.it>
2026-06-30 10:14:13 +0200

    modifica parameti title e user

--- app/models/event.py
+++ app/models/event.py
@@ -1,12 +1,15 @@
 from sqlmodel import SQLModel, Field
 from datetime import datetime
 from typing import Optional
+from pydantic import ConfigDict
 
 class CreateEvent(SQLModel):
     title : str
     description : str
     date: datetime
     location : str
+
+    model_config = ConfigDict(strict=True)
 
 class Event(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
--- app/models/user.py
+++ app/models/user.py
@@ -1,8 +1,10 @@
+from pydantic import ConfigDict
 from sqlmodel import SQLModel, Field #impostare modello database
 
 
 
 class User(SQLModel, table=True):
+    model_config = ConfigDict(strict=True)
     username : str = Field(primary_key=True) #collegamento class user con registration
     name : str
     email: str
