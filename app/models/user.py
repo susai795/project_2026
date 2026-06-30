61a875d86602a5347baaa12b7ced5855409267a8
susai795 <s.usai151@studenti.unica.it>
2026-06-30 10:19:50 +0200

    modifiche

--- app/models/user.py
+++ app/models/user.py
@@ -4,7 +4,7 @@ from sqlmodel import SQLModel, Field #impostare modello database
 
 
 class User(SQLModel, table=True):
-    model_config = ConfigDict(strict=True)
     username : str = Field(primary_key=True) #collegamento class user con registration
     name : str
     email: str
+    model_config = ConfigDict(strict=True)
